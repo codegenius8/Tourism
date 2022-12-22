a2895970c4ac80b8e85c7087c59a511cdd0ca67a
codegenius8 <94888474+codegenius8@users.noreply.github.com>
2022-12-22 11:51:54 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -44,7 +44,7 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || 'An unknown error occurred here!' });
 });
 
 mongoose
