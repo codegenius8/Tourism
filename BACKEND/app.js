595dfda137a7e5a1965fd185275fb86195b4e69b
codegenius8 <94888474+codegenius8@users.noreply.github.com>
2023-01-18 10:48:32 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
-
+// set header
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
